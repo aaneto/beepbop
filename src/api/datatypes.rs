c8947d3a10ae38c32a4c6637d8b3b12242d86167
Adilson Neto <almeidneto@gmail.com>
2019-04-13 18:59:18 -0300

    Add ChatMember data type

--- src/api/datatypes.rs
+++ src/api/datatypes.rs
@@ -175,3 +175,23 @@ impl FileBuffer {
         FileBuffer { name, data }
     }
 }
+
+#[derive(Debug, Deserialize)]
+pub struct ChatMember {
+    user: User,
+    status: String,
+    until_date: Option<u64>,
+    can_be_edited: Option<bool>,
+    can_change_info: Option<bool>,
+    can_post_messages: Option<bool>,
+    can_edit_messages: Option<bool>,
+    can_delete_messages: Option<bool>,
+    can_invite_users: Option<bool>,
+    can_restrict_members: Option<bool>,
+    can_pin_messages: Option<bool>,
+    can_promote_members: Option<bool>,
+    can_send_messages: Option<bool>,
+    can_send_media_messages: Option<bool>,
+    can_send_other_messages: Option<bool>,
+    can_add_web_page_previews: Option<bool>,
+}
