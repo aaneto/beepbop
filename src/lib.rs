54a8bbdd0eabc06ca87ab3dea748a933d1c9cc46
Adilson Neto <almeidneto@gmail.com>
2019-04-13 19:02:50 -0300

    Fix test to use get_argv function to get API_KEY by a environment variable

--- src/lib.rs
+++ src/lib.rs
@@ -26,15 +26,13 @@ mod tests {
     //! for bots. That includes running actual API
     //! calls and expecting an return.
     use crate::api::Bot;
+    use crate::util::get_argv;
     use std::error::Error;
     use tokio::runtime::Runtime;
 
     #[test]
     fn test_get_me() {
-        let (_, api_key) = std::env::vars()
-            .find(|(key, _)| key == "API_KEY")
-            .expect("Cannot find API_KEY in ENV");
-
+        let api_key = get_argv("API_KEY").expect("Cannot find API_KEY in ENV");
         let bot = Bot::new(&api_key);
 
         let mut runtime = Runtime::new().expect("Unable to create a runtime");
