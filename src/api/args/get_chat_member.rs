8695d5660f92204138f49b0b035c4655f950da34
Adilson Neto <almeidneto@gmail.com>
2019-04-20 02:46:32 -0300

    Implement 'new' method to GetChatMember

--- src/api/args/get_chat_member.rs
+++ src/api/args/get_chat_member.rs
@@ -2,9 +2,15 @@ use serde_derive::Serialize;
 
 use crate::api::args::ChatID;
 
-#[derive(Default, Debug, Serialize)]
+#[derive(Debug, Serialize)]
 pub struct GetChatMember {
     #[serde(flatten)]
     pub chat_id: ChatID,
     pub user_id: i64,
+}
+
+impl GetChatMember {
+    pub fn new(chat_id: ChatID, user_id: i64) -> Self {
+        Self { chat_id, user_id }
+    }
 }
