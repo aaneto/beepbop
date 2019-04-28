6e2d3a29fdd754277e6dace8ad465ec30c1ccbfe
Adilson Neto <almeidneto@gmail.com>
2019-04-28 16:51:35 -0300

    Lint file_buffer.rs with cargo fmt

--- src/api/datatypes/file_buffer.rs
+++ src/api/datatypes/file_buffer.rs
@@ -1,18 +1,21 @@
+use std::error::Error;
+use std::path::Path;
 
-use crate::api::error::APIError;
+
 use futures::Future;
 use tokio::io::AsyncWrite;
-use std::path::Path;
-use std::error::Error;
+
+use crate::api::error::APIError;
 
 /// A FileBuffer is a struct
 /// representing a file returned
 /// from telegram. It has a filename
 /// and a byte stream.
-/// 
+///
 /// There are no guarantees about meta data
 /// from telegram, so name can be ignored when
 /// saving the file locally.
+#[derive(Debug)]
 pub struct FileBuffer {
     pub data: Vec<u8>,
     pub name: String,
@@ -25,17 +28,13 @@ impl FileBuffer {
     }
 
     /// Save the file buffer to a file path.
-    pub fn save_as<P>(self, path: P) -> impl Future<Item=(), Error=APIError>
+    pub fn save_as<P>(self, path: P) -> impl Future<Item = (), Error = APIError>
     where
-        P: AsRef<Path> + Send + 'static
+        P: AsRef<Path> + Send + 'static,
     {
         tokio::fs::File::create(path)
-            .and_then(move |mut file| {
-                file.poll_write(&self.data)
-            })
+            .and_then(move |mut file| file.poll_write(&self.data))
             .map(|_| ())
-            .map_err(|err| {
-                APIError::DownloadError(err.description().to_owned())
-            })
+            .map_err(|err| APIError::DownloadError(err.description().to_owned()))
     }
 }
