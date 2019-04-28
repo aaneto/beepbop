d4ee50d16128d6cc77ec5643ddd9bc4a4c5504ee
Adilson Neto <almeidneto@gmail.com>
2019-04-28 18:28:40 -0300

    Add a test for get_chat

--- src/api/methods/get_chat.rs
+++ src/api/methods/get_chat.rs
@@ -20,3 +20,28 @@ impl Bot {
             .execute()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::api::Bot;
+    use crate::util::get_argv;
+    use tokio::runtime::Runtime;
+
+    #[test]
+    fn test_get_chat() {
+        let api_key = get_argv("API_KEY").expect("Cannot find API_KEY in ENV");
+
+        let chat_id: i64 = get_argv("CHAT_ID")
+            .expect("Cannot find CHAT_ID in ENV")
+            .parse()
+            .expect("CHAT_ID is not an valid ID.");
+
+        let bot = Bot::new(&api_key);
+
+        let mut runtime = Runtime::new().expect("Unable to create a runtime");
+
+        if let Err(err) = runtime.block_on(bot.get_chat(chat_id)) {
+            panic!("{:#?}", err);
+        }
+    }
+}
