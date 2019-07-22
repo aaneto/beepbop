95ead5027ce1ae22b6f2ac6edb7b6e149cd1d199
Adilson Neto <almeidneto@gmail.com>
2019-07-21 22:40:36 -0300

    Add initial stream implementation

--- src/stream.rs
+++ src/stream.rs
@@ -1,75 +1,85 @@
 //! The Stream module provides utilities for
 //! dealing with streams of bot updates.
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
-use std::sync::mpsc;
 
-use futures::Future;
 use tokio::runtime::Runtime;
 
 use crate::bot::Bot;
+use crate::error::StreamError;
+use crate::input::GetUpdates;
 use crate::object::Message;
 use crate::object::Update;
 use crate::object::UpdateKind;
-use crate::input::GetUpdates;
 
+#[cfg(feature = "stream-logging")]
+use log::info;
 
-type UpdateSender = mpsc::Sender<Vec<Update>>;
+type MessageSender = mpsc::Sender<Message>;
 
 pub struct Stream {
     bot: Bot,
-    senders: Vec<UpdateSender>,
+    message_sender: Option<MessageSender>,
 }
 
 impl Stream {
     pub fn new(bot: &Bot) -> Stream {
         Stream {
             bot: bot.clone(),
-            senders: vec![],
+            message_sender: None,
         }
     }
 
-    pub fn for_each_message<'a, Fut, Func>(&mut self, closure: Func)
+    pub fn for_each_message<Func>(&mut self, closure: Func)
     where
-        Fut: Future<Item = (), Error = ()> + 'static + Send,
-        Func: Fn(Bot, Message) -> Fut + 'static + Send
+        Func: Send + 'static + Fn(&mut Runtime, Bot, Message) -> (),
     {
         let (sender, receiver) = mpsc::channel();
 
         let thread_bot = self.bot.clone();
 
-        self.senders.push(sender);
+        self.message_sender = Some(sender);
 
         thread::spawn(move || {
             let mut runtime = Runtime::new().expect("Unable to create a runtime");
 
-            for updates in receiver {
-                for update in updates {
-                    if let UpdateKind::Message(message) = update.data {
-                        runtime.block_on(closure(thread_bot.clone(), message)).is_ok();
-                    }
-                }
+            for message in receiver {
+                #[cfg(feature = "stream-logging")]
+                info!("Message streamer received: {:?}", message);
+
+                closure(&mut runtime, thread_bot.clone(), message);
             }
         });
     }
 
-    pub fn run(self) {
+    pub fn run(self) -> Result<(), StreamError> {
+        #[cfg(feature = "stream-logging")]
+        env_logger::init();
+
         let mut update_offset: Option<i64> = None;
         let mut runtime = Runtime::new().expect("Unable to create a runtime");
 
         loop {
             let mut get_updates_args = GetUpdates::new();
             get_updates_args.offset = update_offset;
-            println!("FETCHING UPDATES OFFSET: {:?}", get_updates_args);
 
-            let (_, updates) = runtime.block_on(self.bot.to_owned().get_updates(get_updates_args)).unwrap();
+            #[cfg(feature = "stream-logging")]
+            info!("Fetching {:?}", get_updates_args);
 
-            update_offset = updates.last().map(|last_update: &Update| {
-                last_update.update_id + 1
-            });
+            let (_, updates) =
+                runtime.block_on(self.bot.to_owned().get_updates(get_updates_args))?;
 
-            for sender in self.senders.iter() {
-                sender.send(updates.clone()).unwrap();
+            update_offset = updates
+                .last()
+                .map(|last_update: &Update| last_update.update_id + 1);
+
+            for update in updates {
+                if let (UpdateKind::Message(message), Some(sender)) =
+                    (update.data, self.message_sender.as_ref())
+                {
+                    sender.send(message)?;
+                }
             }
 
             std::thread::sleep(Duration::from_secs(1));
